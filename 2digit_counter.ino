80e4b37cc4fbb90c891c042030190a10e821ee80
SpiritasX <32173996+SpiritasX@users.noreply.github.com>
2019-09-29 12:28:53 +0200

    Update 2digit_counter.ino

--- 2digit_counter.ino
+++ 2digit_counter.ino
@@ -1,3 +1,4 @@
+//Pins for first display
 int jedanNula[] = {10, 11, 12, A0, A1, A2};
 int jedanJedan[] = {10, A2};
 int jedanDva[] = {10, 11, 13, A0, A1};
@@ -9,6 +10,7 @@ int jedanSedam[] = {10, 11, A2};
 int jedanOsam[] = {10, 11, 12, 13, A0, A1, A2};
 int jedanDevet[] = {10, 11, 12, 13, A1, A2};
 
+//Pins for second display
 int dvaNula[] = {6, 7, 8, A3, A4, A5};
 int dvaJedan[] = {6, A5};
 int dvaDva[] = {6, 7, 9, A3, A4};
@@ -20,8 +22,10 @@ int dvaSedam[] = {6, 7, A5};
 int dvaOsam[] = {6, 7, 8, 9, A3, A4, A5};
 int dvaDevet[] = {6, 7, 8, 9, A4, A5};
 
+//Counters
 int k = 0, j = 0;
 
+//Sets all pins to HIGH which means that no current is going through displays
 void setup() {
   for (int i = 6; i < 14; i++) {
     pinMode(i, OUTPUT);
@@ -42,13 +46,14 @@ void setup() {
   digitalWrite(A5, HIGH);
 }
 
+//Function for changing the pins to LOW depending on which number has to show up
 void desetica() {
   if (k == 0) {
     for (int i = 0; i < 6; i++) {
       digitalWrite(jedanNula[i], LOW);
       delay(1);
     }
-    return;
+    return;  //after every number, returns to loop() function
   } else if (k == 1) {
     for (int i = 0; i < 2; i++) {
       digitalWrite(jedanJedan[i], LOW);
@@ -102,12 +107,13 @@ void desetica() {
       digitalWrite(jedanDevet[i], LOW);
       delay(1);
     }
-    if(j == 9 && k == 9)
+    if(j == 9 && k == 9)  //checks if both numbers are showing 9 and sets the first digit to 0
       k = -1;
     return;
   }
 }
 
+//function for second display
 void jedinica() {
   if (j == 0) {
     for (int i = 0; i < 6; i++) {
@@ -168,13 +174,14 @@ void jedinica() {
       digitalWrite(dvaDevet[i], LOW);
       delay(1);
     }
-    j = -1;
-    k++;
+    j = -1;  //sets the second display to 0
+    k++;     //every 10 iterations of this function, counts once for first display
     return;
   }
 }
 
 void loop() {
+  //after every return resets all the pins to HIGH
   for (int i = 6; i < 14; i++) {
     digitalWrite(i, HIGH);
     delay(1);
@@ -185,8 +192,11 @@ void loop() {
   digitalWrite(A3, HIGH);
   digitalWrite(A4, HIGH);
   digitalWrite(A5, HIGH);
+  //calls for both functions
   desetica();
   jedinica();
+  //counter for second display
   j++;
-  delay(250);
+  //counts every number as one second. Can be changed to lower value for faster iterations or reverse.
+  delay(1000);
 }
